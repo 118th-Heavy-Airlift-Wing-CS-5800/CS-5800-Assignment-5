32f51afc481e06410443a2c035af59c1e62c53a8
unknown <sunjayg36@gmail.com>
2025-07-08 17:11:38 -0700

    Put all files in main folder

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 }); 
 
+
 const budgetRoutes = require('./routes/budgetRoutes');
 app.use('/api/budgets', budgetRoutes);
 
